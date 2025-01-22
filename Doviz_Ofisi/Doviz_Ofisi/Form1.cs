4e833a343ada8ed097fbd5416159c1c7c8ab7a36
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-22 10:46:33 +0300

    Exchange Office Project with XML and SQL

---
+++ Doviz_Ofisi/Doviz_Ofisi/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Data.SqlClient;
+
+namespace Doviz_Ofisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=Odeux\\MSSQLSERVER01;Initial Catalog=DbDoviz;Integrated Security=True;TrustServerCertificate=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string bugün = "https://tcmb.gov.tr/kurlar/today.xml";
+            var xmldosya = new XmlDocument();
+            xmldosya.Load(bugün);
+
+            string dolaralis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteBuying").InnerXml;
+            lblDolarAlis.Text = dolaralis;
+
+            string dolarsatis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            lblDolarSatis.Text = dolarsatis;
+
+            string euroalis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteBuying").InnerXml;
+            lblEuroAlis.Text = dolaralis;
+
+            string eurosatis = xmldosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+            lblEuroSatis.Text = dolarsatis;
+        }
+
+        private void btnDolarAlis_Click(object sender, EventArgs e)
+        {
+            txtKur.Text = lblDolarAlis.Text;
+        }
+
+        private void btnDolarSatis_Click(object sender, EventArgs e)
+        {
+            txtKur.Text = lblDolarSatis.Text;
+        }
+
+        private void btnEuroAlis_Click(object sender, EventArgs e)
+        {
+            txtKur.Text = lblEuroAlis.Text;
+        }
+
+        private void btnEuroSatis_Click(object sender, EventArgs e)
+        {
+            txtKur.Text = lblEuroSatis.Text;
+        }
+
+        private void btnSatisYap_Click(object sender, EventArgs e)
+        {
+            double kur, miktar, tutar;
+            kur = Convert.ToDouble(txtKur.Text);
+            miktar = Convert.ToDouble(txtMiktar.Text);
+            tutar = kur * miktar;
+            txtTutar.Text = tutar.ToString();
+
+        }
+
+        private void txtKur_TextChanged(object sender, EventArgs e)
+        {
+            txtKur.Text = txtKur.Text.Replace(",",".");
+        }
+
+        private void btnSatisYap2_Click(object sender, EventArgs e)
+        {
+            double kur = Convert.ToDouble(txtKur.Text);
+            int miktar = Convert.ToInt16(txtMiktar.Text);
+            int tutar = Convert.ToInt16(miktar / kur);
+            txtTutar.Text = tutar.ToString();
+            double kalan;
+            kalan = miktar % kur;
+            txtKalan.Text = kalan.ToString();
+
+        }
+
+        // Veritabanı işlemleri
+
+        private void btnData1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO TBLDoviz (MİKTAR,TUTAR) values (@p1,@p2)", baglanti);
+            komut.Parameters.AddWithValue("@p1", Convert.ToDouble(txtMiktar.Text));
+            komut.Parameters.AddWithValue("@p2", Convert.ToDouble(txtTutar.Text));
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("İşlem Kaydedildi");
+        }
+
+        public void btnData2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO TBLDoviz (MİKTAR,KALAN,TUTAR) values (@p1,@p2,@p3)", baglanti);
+            komut.Parameters.AddWithValue("@p1", Convert.ToDouble(txtMiktar.Text));
+            komut.Parameters.AddWithValue("@p2", Convert.ToDouble(txtTutar.Text));
+            komut.Parameters.AddWithValue("@p3", Convert.ToDouble(txtKalan.Text));
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("İşlem Kaydedildi");
+        }
+
+
+    }
+}
