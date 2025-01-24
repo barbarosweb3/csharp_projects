dfa869bc9d743b6e5ce97f09f88287837609303f
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-24 14:16:03 +0300

    Half Project
    The rest of it will be completed once ATM project is done.

---
+++ Shopping_Data/Shopping_Data/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Shopping_Data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BtnData = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(38, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1057, 595);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ÜRÜN SATIŞ BİLGİSİ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1051, 568);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // BtnData
+            // 
+            this.BtnData.Location = new System.Drawing.Point(1142, 207);
+            this.BtnData.Name = "BtnData";
+            this.BtnData.Size = new System.Drawing.Size(241, 69);
+            this.BtnData.TabIndex = 1;
+            this.BtnData.Text = "DATADAKİ BİLGİLERİ GETİR";
+            this.BtnData.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1429, 655);
+            this.Controls.Add(this.BtnData);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button BtnData;
+    }
+}
+
---
+++ Shopping_Data/Shopping_Data/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Shopping_Data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=Odeux\\MSSQLSERVER01;Initial Catalog=Test;Integrated Security=True;TrustServerCertificate=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand cmd = new SqlCommand("SELECT HAREKETID, URUNAD, \r\n       (AD + ' ' + SOYAD) AS 'MÜŞTERİ', \r\n       (ADSOYAD) AS 'PERSONEL', \r\n       TBLURUNLER.FIYAT as 'FİYAT' \r\nFROM TBLHAREKET \r\nLEFT JOIN TBLURUNLER ON TBLHAREKET.URUN = TBLURUNLER.URUNID\r\nLEFT JOIN TBLMUSTERI ON TBLHAREKET.MUSTERI = TBLMUSTERI.ID\r\nLEFT JOIN TBLPERSONEL ON TBLHAREKET.PERSONEL = TBLPERSONEL.ID\r\n", baglanti);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
+
+    }
+}
